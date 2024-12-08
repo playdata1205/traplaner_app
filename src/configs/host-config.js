e901da52b36d657d686ed97be8a351361ce206cf
maru423 <jason423@naver.com>
2024-12-08 22:04:03 +0900

    deploy.yml 변경

--- src/configs/host-config.js
+++ src/configs/host-config.js
@@ -4,7 +4,7 @@ let clientHostName = window.location.hostname;
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:8181'; // 로컬 backend 서버 주소
 } else if (clientHostName === 'traplaner.site') {
-  backendHostName = 'https://localhost:8181'; // 배포 backend 서버 주소 추후 변경 필
+  backendHostName = 'https://localhost:8181'; // 배포 backend 서버 주소 추후 변경 필요
 }
 
 export const API_BASE_URL = backendHostName;
