779b688fad9a8a856a8233f54da70fa5574788e0
CodeName-maru <jason423@naver.com>
2024-12-09 20:21:15 +0900

    host-config 변경

--- src/configs/host-config.js
+++ src/configs/host-config.js
@@ -1,15 +1,15 @@
+const clientHostName = window.location.hostname;
 let backendHostName;
-let clientHostName = window.location.hostname;
 
 if (clientHostName === 'localhost') {
   backendHostName = 'http://localhost:8181'; // 로컬 backend 서버 주소
-} else if (clientHostName === 'traplaner.site') {
-  backendHostName = 'api.traplaner.site'; // 배포 backend 서버 주소 추후 변경 필요
+} else if (clientHostName === 'traplan.site') {
+  backendHostName = 'https://api.traplan.site'; // 배포 backend 서버 주소 추후 변경 필요
 }
 
 export const API_BASE_URL = backendHostName;
-export const MEMBER = 'member-service';
-export const TRAVELBOARD = 'travelboard-service';
-export const TRAVELPLAN = 'travelplan-service';
-export const MYPAGE = 'mypage-service';
-export const MAIN = 'main-service';
+export const MEMBER = '/member-service';
+export const TRAVELBOARD = '/travelboard-service';
+export const TRAVELPLAN = '/travelplan-service';
+export const MYPAGE = '/mypage-service';
+export const MAIN = '/main-service';
